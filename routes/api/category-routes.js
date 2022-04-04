d65cf943bc460662cfac4c97fac8d5876a8c500e
Sawyer Silfies <ssilfies17@gmail.com>
2022-04-03 20:53:17 -0700

    Tag model requirments, category routes started

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,18 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      fields: ['id'],
+      include: [{ model: Product }]
+    });
+
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
